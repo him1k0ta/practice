1c0a96dd1f8dad207d262669f203850b89f78977
him1k0ta <149035975+him1k0ta@users.noreply.github.com>
2025-05-20 18:06:28 +0700

    new

---
+++ script/script.js
@@ -0,0 +1,31 @@
+function encode() {
+    let abc = 'abcdefghijklmnopqrstuvwxyz';
+    let ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let text = document.getElementById('inputArea').value;
+    let output = '';
+
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i];
+        let found = false;
+
+        for (let j = 0; j < abc.length; j++) {
+            if (char === abc[j] || char === ABC[j]) {
+                let newPos = (j + 13) % 26;
+                
+                if (char === abc[j]) {
+                    output += abc[newPos];
+                } else {
+                    output += ABC[newPos];
+                }
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            output += char;
+        }
+    }
+
+    document.getElementById('outputArea').value = output;
+}
